a546e58d84b64c8dfb93454138275cd33cdaa43e
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2024-07-20 10:43:42 -0300

    Adding initial tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,11 @@
-// import {
-//   assertEquals,
-//   assertNotEquals,
-// } from 'https://deno.land/std@0.224.0/assert/mod.ts'
-// import { memoize, compareValues } from '../src/curry.ts'
+import { assertEquals, assertInstanceOf } from 'https://deno.land/std@0.224.0/assert/mod.ts';
 
-// Deno.test('Comparing values', () => {
-//     // ...
-//   },
-// )
+import { curry } from '../src/curry.ts';
+
+Deno.test('curry()', () => {
+  const sum = curry((a: number, b: number) => a + b)
+
+  assertInstanceOf(sum(1), Function)
+  assertEquals(sum(1).length, 1)
+  assertEquals(sum(1)(2), 3)
+})
